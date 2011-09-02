f4c60d04cea0ee054aa727de553a5e2d7148b364
Rob Allen <rob@akrabat.com>
2011-09-02 20:45:53 +0100

    Information files

---
+++ modules/DefaultFavicon/Information.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This class is consumed by Zend\Module\InformationConsumer and allows
+ * the module developer to specify if and how this module is to be wired up.
+ * 
+ */
+
+namespace DefaultFavicon;
+
+Class Information
+{
+    
+}
---
+++ modules/Markdown/Information.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This class is consumed by Zend\Module\InformationConsumer and allows
+ * the module developer to specify if and how this module is to be wired up.
+ * 
+ */
+
+namespace Markdown;
+
+Class Information
+{
+}
